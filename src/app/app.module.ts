c3913ffdd1e9add64017732bc7a867136caecf93
JAzzad5 <carlosjosue0114@gmail.com>
2021-11-01 15:16:52 -0600

    [ADD] creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
+import { BarraLateralComponent } from './components/barra-lateral/barra-lateral.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    InicioSesionComponent,
+    BarraLateralComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/barra-lateral/barra-lateral.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-lateral',
+  templateUrl: './barra-lateral.component.html',
+  styleUrls: ['./barra-lateral.component.css']
+})
+export class BarraLateralComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
