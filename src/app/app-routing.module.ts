88df680f90eaa959470672e61682c1e2b83f064a
JAzzad5 <carlosjosue0114@gmail.com>
2021-11-04 15:57:30 -0600

    [ADD] Landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 const routes: Routes = [
-  {path: 'dashboard', component:DashboardComponent}
+  {path: 'dashboard', component:DashboardComponent},
+  {path: '', component:LandingPageComponent}
 ];
 
 @NgModule({
