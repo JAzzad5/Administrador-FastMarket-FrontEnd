75131e44a9ab656d3af3dc242900774bdfb36c31
JAzzad5 <carlosjosue0114@gmail.com>
2021-11-04 16:19:49 -0600

    [ADD] componente de inicio de sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 const routes: Routes = [
   {path: 'dashboard', component:DashboardComponent},
-  {path: '', component:LandingPageComponent}
+  {path: '', component:LandingPageComponent},
+  {path: 'inicio-sesion', component: InicioSesionComponent}
 ];
 
 @NgModule({
