3d533c8ba6e280dc403fb249d9086a780c09f30e
JAzzad5 <carlosjosue0114@gmail.com>
2021-11-30 20:50:01 -0600

    [ADD] login admin

--- src/app/components/inicio-sesion/inicio-sesion.component.ts
+++ src/app/components/inicio-sesion/inicio-sesion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+declare const Swal: any;
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioSesionComponent implements OnInit {
 
+  
+  formularioLogin = new FormGroup({
+    Usuario:new FormControl('', [Validators.required]),
+    Contraseña:new FormControl(''),
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
+  iniciarSesion(){
+    console.log(this.formularioLogin.value);
+    console.log(this.formularioLogin.value.Usuario);
+    if(this.formularioLogin.value.Usuario == 'admin' && this.formularioLogin.value.Contraseña == 'admin' ){
+      window.location.href = '/dashboard';
+    }
+    else{
+      this.incorrecto();
+    }
+  }
+
+
+  
+  incorrecto(){
+    Swal.fire({
+      position: 'center',
+      icon: 'error',
+      title: `Usuario y contraseña no coinciden.`,
+      showConfirmButton: false,
+      timer: 2500,
+    });
+  }
+  
 
 }
